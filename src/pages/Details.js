f4052e64862daa92bbadd8d8c640621dd8d5ab5d
Thiago Felipe <tfeliperibeiro@pop-os.localdomain>
2021-07-18 17:13:06 -0300

    feat: Implementando cards de detalhes

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -4,10 +4,22 @@ import Context from '../context/context';
 
 const Details = ({ match: { params: { id } } }) => {
   const { data } = useContext(Context);
+
   return (
     <div>
       {data.filter((hero) => hero.id === Number(id))
-        .map((hero) => <p key={hero.id}>{hero.alias}</p>)}
+        .map((hero) => (
+          <section key={hero.id}>
+            <img src={hero.img} alt={`Imagem ${hero.alias}`} />
+            <p>{`Descrição: ${hero.description}`}</p>
+            <ul>
+              <li>{`Apelido: ${hero.alias}`}</li>
+              <li>{`Nome: ${hero.name}`}</li>
+              <li>{`Equipe: ${hero.affiliation}`}</li>
+            </ul>
+            <button type="button">Editar</button>
+          </section>
+        ))}
     </div>
   );
 };
