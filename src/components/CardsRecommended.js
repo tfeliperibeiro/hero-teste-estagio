3f0f3894a0e9a9835c678f3c36d63c603362e4e2
Thiago Felipe <tfeliperibeiro@pop-os.localdomain>
2021-07-18 16:32:11 -0300

    fix: Organizando componentes

--- src/components/CardsRecommended.js
+++ src/components/CardsRecommended.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Context from '../context/context';
+
 import '../css/home.css';
 
 const CardsRecommended = () => {
