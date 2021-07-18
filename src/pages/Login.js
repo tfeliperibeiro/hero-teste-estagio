b5777e295febd5b26924077f24e9d99bde60e162
Thiago Felipe <tfeliperibeiro@pop-os.localdomain>
2021-07-18 14:33:50 -0300

    fix: retirando div sem uso da pagina login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,21 +8,19 @@ const Login = () => {
   const { handleInput } = useContext(context);
   return (
     <main>
-      <div>
-        <div className="container-login">
-          <section className="text-main">
-            <h2>A melhor plataforma de</h2>
-            <h2 className="text-hero">HERÓIS.</h2>
-            <input
-              onChange={handleInput}
-              placeholder="Entre com seu nome"
-              className="input-login"
-              type="text"
-            />
-            <Link to="/home" className="btn-login" type="button">Entrar</Link>
-          </section>
-          <img className="avatar" src={avatar} alt="Imagem Harry Potter" />
-        </div>
+      <div className="container-login">
+        <section className="text-main">
+          <h2>A melhor plataforma de</h2>
+          <h2 className="text-hero">HERÓIS.</h2>
+          <input
+            onChange={handleInput}
+            placeholder="Entre com seu nome"
+            className="input-login"
+            type="text"
+          />
+          <Link to="/home" className="btn-login" type="button">Entrar</Link>
+        </section>
+        <img className="avatar" src={avatar} alt="Imagem Harry Potter" />
       </div>
     </main>
 
