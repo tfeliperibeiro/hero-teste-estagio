d880b5867942226173a7996158b188026bb381d9
Thiago Felipe <tfeliperibeiro@pop-os.localdomain>
2021-07-18 16:30:07 -0300

    fix: Organizando os componentes de cards

---
+++ src/components/CardsRecommended.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import Context from '../context/context';
+import '../css/home.css';
+
+const CardsRecommended = () => {
+  const { data } = useContext(Context);
+
+  return (
+    <div>
+      <h2 className="text-recommended">Recomendados por nós</h2>
+      <section className="container-cards">
+        {data && data.filter((hero) => hero.alias !== '')
+          .map((hero) => (
+            <div className="card-hero" key={hero.id}>
+              <img className="img-card" src={hero.img} alt={hero.name} />
+              <p>{hero.alias}</p>
+              <Link
+                className="btn-details"
+                to={`/home/${hero.id}/detalhes`}
+              >
+                Ver detalhes
+              </Link>
+            </div>
+          ))}
+      </section>
+    </div>
+  );
+};
+
+export default CardsRecommended;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,12 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import CardsRecommended from '../components/CardsRecommended';
 import Header from '../components/Header';
-import Context from '../context/context';
-import '../css/home.css';
 
-const Home = () => {
-  const { data } = useContext(Context);
-
-  return (
-    <div>
-      <Header />
-      <h2 className="text-recommended">Recomendados por nós</h2>
-      <section className="container-cards">
-        {data && data.filter((hero) => hero.alias !== '')
-          .map((hero) => (
-            <div className="card-hero" key={hero.id}>
-              <img className="img-card" src={hero.img} alt={hero.name} />
-              <p>{hero.alias}</p>
-              <Link
-                className="btn-details"
-                to={`/home/${hero.id}/detalhes`}
-              >
-                Ver detalhes
-              </Link>
-            </div>
-          ))}
-      </section>
-    </div>
-  );
-};
+const Home = () => (
+  <div>
+    <Header />
+    <CardsRecommended />
+  </div>
+);
 
 export default Home;
