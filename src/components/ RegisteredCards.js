f419c054476300572d50daad812b1dacdcb6788e
Thiago Felipe <tfeliperibeiro@pop-os.localdomain>
2021-07-19 02:20:51 -0300

    feat: Colocando mensagem quando nao tiver heroi

--- src/components/ RegisteredCards.js
+++ src/components/ RegisteredCards.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Context from '../context/context';
+import imageHero from '../images/hero.png';
 
 import '../css/home.css';
 
@@ -11,8 +12,13 @@ const RegisteredCards = () => {
     <div>
       <h2 className="text-recommended">Heróis cadastrados</h2>
       <section className="container-cards">
-        {saveHero && saveHero.map((hero) => (
+        {saveHero.length ? saveHero.map((hero) => (
           <div className="card-hero" key={hero.name}>
+            <img
+              className="img-card"
+              src={hero.patch}
+              alt={`Imagem de ${hero.name}`}
+            />
             <p>{hero.name}</p>
             <Link
               className="btn-details"
@@ -21,7 +27,7 @@ const RegisteredCards = () => {
               Ver detalhes
             </Link>
           </div>
-        ))}
+        )) : <img className="hero" src={imageHero} alt="hero" />}
       </section>
     </div>
   );
